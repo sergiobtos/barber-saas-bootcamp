16fa8b97bda40b97d2fdd12c6076724a08c9a4de
Sergio Silva <sergiobtos@hotmail.com>
2025-09-15 21:58:03 -0400

    feat: add Avatar, Badge, and Card components; update package dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import { ptBR } from "date-fns/locale"
 import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 //import Link from "next/link"
 /* 
 import { db } from "./_lib/prisma"
@@ -51,14 +54,39 @@ const Home = async () => {
             <SearchIcon />
           </Button>
         </div>
+
         <div className="relative h-[150px] w-full">
           <Image
-            className="rounded-xl object-cover"
+            alt="Banner"
             src="/banner-01.png"
             fill
-            alt="Banner"
+            className="rounded-xl object-cover"
           />
         </div>
+
+        <h2 className="m-6 mb-3 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="/avatar.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">14:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
